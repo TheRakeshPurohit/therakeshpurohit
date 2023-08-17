c3a8482505eca9c34b964b3c3b9a46b23da9d245
Rakesh Purohit <26131905+TheRakeshPurohit@users.noreply.github.com>
2023-08-17 03:59:49 +0000

    Update 4 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div nexrclassName="grid h-screen place-items-center">
+    <div className="grid h-screen place-items-center">
       Welcome to my place! You already have a seat. So, What would you like to
       have?
     </div>
