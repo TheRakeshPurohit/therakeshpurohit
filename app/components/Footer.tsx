2d2b71b329748618b3eeabbed55e78ff29836b94
Rakesh Purohit <26131905+TheRakeshPurohit@users.noreply.github.com>
2023-08-19 06:39:25 +0000

    sticky footer and header

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 flex h-9 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <div className=" flex flex-row justify-center items-center h-10 gap-2 ">
       {/* GitHub  */}
       <a
         className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,13 @@
-import Image from "next/image";
 export default function Footer() {
   return (
-    <div className="fixed top-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <div className=" flex flex-row justify-center items-center h-10 gap-2">
       <a
         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
       >
-        By{" "}
-        <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          className="dark:invert"
-          width={100}
-          height={24}
-          priority
-        />
+        This is header
       </a>
     </div>
   );
