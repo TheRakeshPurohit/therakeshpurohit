9144b1a0ed052aafe11244c1f936cb741f4c5970
Rakesh Purohit <26131905+TheRakeshPurohit@users.noreply.github.com>
2023-08-20 17:14:43 +0000

    first blog content update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <header className="flex flex-row supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
       <Link className="gap-2 p-8" href="/">
-        house🏠
+        🏠
       </Link>
       <Link className="gap-2 p-8" href="/notes">
-        notes✍🏽
+        ✍🏽
       </Link>
       <Link className="gap-2 p-8" href="/whoami">
-        whoami❓
+        ❓
       </Link>
     </header>
   );
