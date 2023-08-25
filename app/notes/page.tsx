c79f263f4d38ef4840a68ab70897719af02ce8d1
Rakesh Purohit <rakeshpurohit909@gmail.com>
2023-08-25 03:42:08 +0000

    WIP: Card

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,13 @@
 const Card = ({ title, date, readTime }) => {
   return (
-    <div className="flex flex-col flex-grow border-3 border-sky-500 gap-3 p-3">
+    <div className="shadow-lg flex flex-col flex-grow gap-3 p-3 ">
+      <div className="flex flex-col items-center justify-center">
       <span>{title}</span>
+      </div>
+      <div className="flex flex-row justify-between">
       <span>{date}</span>
       <span>{readTime}</span>
+      </div> 
     </div>
   );
 };
